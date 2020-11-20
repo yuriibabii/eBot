262cfde684b5864fbe75266bbf52f5b51563c23f
Yurii Babii <yurii.babii.work@gmail.com>
2020-11-20 23:19:52 +0200

    added bot key

---
+++ eBot/Models/AppSettings.cs
@@ -0,0 +1,11 @@
+namespace eBot.Models
+{
+    public static class AppSettings
+    {
+        public const string Url = "https://URL:443/{0}";
+        
+        public const string Name = "eBot";
+        
+        public const string Key = "1237881636:AAF7MHazIE5OJ_5DvoY5EPZBOX85BPzdyA0";
+    }
+}
